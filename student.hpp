49faf70ded128eab14996744960a6d882fd7a43c
MrBarnesTVDSB <90469601+MrBarnesSDCI@users.noreply.github.com>
2021-10-21 18:24:45 -0400

    Add files via upload

---
+++ student.hpp
@@ -0,0 +1,24 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Replace string function
+char exerciseOne(int mark)
+{
+	return '0';
+}
+
+bool exerciseTwo(string word)
+{
+	return 0;
+}
+
+string exerciseThree(string sentence, string word)
+{
+	return "0";
+}
